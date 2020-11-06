ba29e24c8040d139f4140476236d43aca41f36d7
obf1313 <oubaifan@163.com>
2020-11-06 15:48:43 +0800

    优化打包

--- build/optimization.js
+++ build/optimization.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'manifest'
   },
   splitChunks: {
+    maxInitialRequests: 5,
     // 第三方打包
     cacheGroups: {
       default: false,
@@ -14,11 +15,29 @@ module.exports = {
       highlight: {
         name: 'highlight',
         chunks: 'all',
-        priority: 20,
+        priority: 1,
+        test: /highlight.js/
+      },
+      antd: {
+        name: 'antd',
+        chunks: 'all',
+        priority: 2,
         test: function(module) {
-          var context = module.context;
-          return context && (context.indexOf('highlight.js') >= 0)
+          const context = module.context;
+          return context && (
+            context.indexOf('antd') >= 0 ||
+            context.indexOf('@ant-design') >= 0 ||
+            context.indexOf('rc-picker') >= 0 ||
+            context.indexOf('rc-table') >= 0 ||
+            context.indexOf('rc-tree') >= 0
+          );
         }
+      },
+      react: {
+        name: 'react',
+        chunks: 'all',
+        priority: 3,
+        test: /react/
       }
     }
   }
