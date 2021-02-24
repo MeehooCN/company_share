d9a5f58e7dd78cf4f73001dbef09d71c47a8397a
obf1313 <oubaifan@163.com>
2021-02-24 11:06:28 +0800

    修改 tableHook

--- src/components/components/hook/useTableHook.tsx
+++ src/components/components/hook/useTableHook.tsx
@@ -4,6 +4,12 @@
  * @createTime: 2020/9/11 13:12
  **/
 import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+
+interface Sorter {
+  field: string,
+  order: 'orderByDesc' | 'orderByASC'
+}
 
 const paginationInit = {
   current: 1,
@@ -15,21 +21,57 @@ const paginationInit = {
 };
 
 const useTableHook = () => {
+  const history = useHistory();
+  const { state }: any = history.location;
   const [loading, setLoading] = useState<boolean>(false);
-  const [pagination, setPagination] = useState(paginationInit);
-  const [searchContent, setSearchContent] = useState<any>();
-  const handleTableChange = (pagination: any) => {
-    pagination.showTotal = (total: number) => {
-      return `共查询到 ${total} 条数据`;
-    };
-    setPagination(pagination);
+  const [searchContent, setSearchContent] = useState<any>(() => {
+    // 如果是页面返回的，则赋值
+    if (state) {
+      return state.searchContent;
+    } else {
+      return undefined;
+    }
+  });
+  const [pagination, setPagination] = useState(() => {
+    let current: number = 1;
+    let tempPagination: any = { ...paginationInit };
+    if (sessionStorage.getItem('current')) {
+      // @ts-ignore
+      current = parseInt(sessionStorage.getItem('current'), 0);
+    }
+    // 如果是页面返回的，则赋值
+    if (state) {
+      current = state.current;
+    }
+    tempPagination.current = current;
+    return tempPagination;
+  });
+  const [sorter, setSorter] = useState<Sorter>({
+    field: '',
+    order: 'orderByDesc'
+  });
+  // 监听表格变化
+  const handleTableChange = (pagination: any, filters: any, sorter: any, extra: any) => {
+    if (extra.action === 'paginate') {
+      pagination.showTotal = (total: number) => {
+        return `共查询到 ${total} 条数据`;
+      };
+      sessionStorage.setItem('current', pagination.current);
+      setPagination(pagination);
+    } else if (extra.action === 'sort') {
+      setSorter({
+        field: sorter.field,
+        order: sorter.order === 'ascend' ? 'orderByASC' : 'orderByDesc'
+      });
+    }
   };
+  // 搜索
   const handleSearch = (content: any) => {
     pagination.current = 1;
     setPagination(pagination);
     setSearchContent(content);
   };
-  //  删除中调用该方法，解决：删除最后一页最后一条数据时，table表展示空页面bug
+  //  删除中调用该方法，解决：删除最后一页最后一条数据时，table表展示空页面 bug
   const backFrontPage = (lastPageRows: number) => {
     // 如果当前页是最后一页，且最后一页只有1个，则修改 pagination 为上一页
     if (pagination.current === Math.ceil(pagination.total / pagination.pageSize) && lastPageRows === 1 && pagination.current > 1) {
@@ -39,7 +81,7 @@ const useTableHook = () => {
   };
   return {
     loading, setLoading, pagination, setPagination, searchContent, handleTableChange,
-    handleSearch, backFrontPage
+    handleSearch, backFrontPage, sorter
   };
 };
 export default useTableHook;
